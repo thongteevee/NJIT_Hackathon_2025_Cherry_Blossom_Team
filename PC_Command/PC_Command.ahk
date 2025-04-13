0ae7607ef923d493a038b1424492e5b3e430be9a
Thong Khong <thongtv2006@gmail.com>
2025-04-13 11:18:46 -0400

    multiple tab and website, mute master

--- PC_Command/PC_Command.ahk
+++ PC_Command/PC_Command.ahk
@@ -1,1 +1,18 @@
-﻿Run, https://www.youtube.com/watch?v=jsQXgDZIIrY
+﻿SoundSet, 0  ; Mute master volume
+
+websites := []
+websites.Push("https://www.youtube.com/watch?v=jsQXgDZIIrY")
+websites.Push("https://youtu.be/p_di4Zn4wz4?si=SzooNJGQYnN04Bw_&t=630")
+websites.Push("https://njit.instructure.com/courses/44333/files/7668577?module_item_id=1637330")
+
+; Randomly choose between: 1 = website, 2 = Paint, 3 = PDF
+Random, choice, 1, 3
+
+if (choice = 1) {
+    Random, idx, 1, % websites.Length()
+    Run, % websites[idx]
+} else if (choice = 2) {
+    ;Run, mspaint.exe
+} else if (choice = 3) {
+    Run, "C:\Users\Thong Khong\Downloads\ECE251 Student Notes ver6.pdf"
+}
--- PC_Command/PC_Command.js
+++ PC_Command/PC_Command.js
@@ -10,23 +10,18 @@ const port = new SerialPort({ path: COM_PORT, baudRate: BAUD_RATE });
 const parser = new ReadlineParser();
 port.pipe(parser);
 
-let motionHandled = false; // KEEP ONLY FOR THE VIDEO
-
 parser.on("data", (line) => {
     const trimmedLine = line.trim();
     console.log("Sensor says:", trimmedLine);
 
-    if (trimmedLine.includes("[MOTION]") && !motionHandled) {       //KEEP ONLY FOR THE VIDEO
-        motionHandled = true;       // KEEP ONLY FOR THE VIDEO
+    if (trimmedLine.includes("[MOTION]")) {
 
-        // ✅ Run the AHK script
         exec('"M:\\HACKATHON\\PC_Command\\PC_Command.ahk"');
 
-        // ✅ Show Windows notification
         notifier.notify({
             title: '🚨 Motion Detected',
             message: 'Someone triggered the ESP32 sensor!',
-            icon: path.join("C:\\Users\\Thong Khong\\Downloads", "your-image.jpg"), // Use .ico or .png for best result
+            icon: path.join("C:\\Users\\Thong Khong\\Downloads", "your-image.jpg"), // replace with your image path and name
             sound: true,
         });
     }
